8f49e6f08ae90198ebc2c3987f7d93e9355e002d
Ronaksharma24 <shrm_ronak@yahoo.co.uk>
2022-01-04 21:23:13 +0000

    my first commit

---
+++ ManualTesting.java
@@ -0,0 +1,64 @@
+package com.myfirstprogram;
+
+public class ManualTesting {
+
+     public static void main(String[] args){
+         System.out.println("WEEK 1");
+
+         System.out.println("1.what is software?");
+         System.out.println("2.software risks");
+         System.out.println("3.what is software testing");
+         System.out.println("4.benefits of software testing?");
+         System.out.println("5.who does testing");
+         System.out.println("6.qualification of a tester");
+         System.out.println("7.what is manual testing?");
+         System.out.println("8.SDLC");
+         System.out.println("9.STLC");
+
+         System.out.println("WEEK 2");
+
+         System.out.println("1.challenges of manual testing");
+         System.out.println("2.principles of software testing");
+         System.out.println("3.test plan and it's benefits");
+         System.out.println("4.test plan template");
+         System.out.println("5.test case + template");
+         System.out.println("6.test scenario v/s test case");
+         System.out.println("7.testing techniques");
+
+         System.out.println("WEEK 3");
+
+         System.out.println("1.what is bug?");
+         System.out.println("2.Bug lifecycle");
+         System.out.println("3.Bug template");
+         System.out.println("4.severity v/s priority");
+         System.out.println("5.testing levels");
+         System.out.println("  white box testing");
+         System.out.println("  black box testing");
+
+         System.out.println("WEEK 4");
+
+         System.out.println("1.static v/s dynamic testing");
+         System.out.println("2.exploratory testing ");
+         System.out.println("3.Ad-hoc testing");
+         System.out.println("4.software development models");
+         System.out.println("  > waterfall");
+         System.out.println("  >iterative");
+         System.out.println("  >V-model");
+         System.out.println("  >Verification and Validation" );
+         System.out.println("  >Agile");
+         System.out.println("  >Definition of ready/done");
+
+         System.out.println("WEEK 5");
+         System.out.println("1.what is TDD?");
+         System.out.println("2.what is BDD?");
+         System.out.println("  > BDD keywords");
+         System.out.println("  > How to write test cases with BDD?");
+         System.out.println("3.Maintenance");
+
+
+
+
+
+
+     }
+}
