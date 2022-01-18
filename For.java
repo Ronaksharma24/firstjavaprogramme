9c9e8510b2db826b27981121e57d807867c36ddc
Ronaksharma24 <shrm_ronak@yahoo.co.uk>
2022-01-18 19:19:21 +0000

    new commit

---
+++ For.java
@@ -0,0 +1,10 @@
+package com.myfirstprogram;
+
+public class For {
+
+    public static void main (String[] args) {
+
+        for (int num=0;num <=10;num++){
+            System.out.println(num);
+        }
+    }}
--- hello.java
+++
@@ -1,8 +0,0 @@
-package com.myfirstprogram;
-
-public class hello {
-
-    public static void main (String[] args){
-        System.out.println("hello git");
-    }
-}
